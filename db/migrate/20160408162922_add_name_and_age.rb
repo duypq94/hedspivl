19560892b3ff7ccd50aa8e52401d11f93bf5bd0a
HustMaroon <thanhbk6294@gmail.com>
2016-04-09 16:18:36 +0700

    edit migration

--- db/migrate/20160408162922_add_name_and_age.rb
+++ db/migrate/20160408162922_add_name_and_age.rb
@@ -1,8 +1,6 @@
 class AddNameAndAge < ActiveRecord::Migration
   def change
-
-
-    add_column :users, :email, :string
+  	add_column :users, :name, :string
 	add_column :users, :age, :integer
     add_index :users, :name, unique: true
     
